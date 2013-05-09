955c213eaa51bbe9ea004dd74b8de06bbbd391a0
James Kong <fishkingsin@gmail.com>
2013-05-09 10:32:21 +0800

    update strip style

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -8,13 +8,16 @@ void testApp::setup(){
     for (int j=0; j<NUM_STRIP; j++)
     {
         pos[j].set(ofRandom(0, ofGetWidth()),ofRandom(0,ofGetHeight()),0);
-        
+        vec[j].set(0,0,0);
+        acc[j].set(ofRandom(-1,1),ofRandom(-1,1),0);
+        age[j] = 1;
+        float h = ofRandom(200,255);
         for (int i=0; i<LENGTH; i++)
         {
             int index = i+(j*LENGTH);
             strip[index].set(ofGetWidth()*0.5,ofGetHeight()*0.5,0);
             float brightness = sinf(PI*float((i*0.5)*1.f/LENGTH*0.5f))*255;
-            color[index].set(ofColor::fromHsb(brightness,0, 255,brightness));
+            color[index].set(ofColor::fromHsb(h,255, 255,brightness));
         }
         
         for (int i=0; i<LOC_LENGTH; i++)
@@ -29,7 +32,8 @@ void testApp::setup(){
     vbo.setVertexData(strip, total, GL_DYNAMIC_DRAW);
 	vbo.setColorData(color, total, GL_DYNAMIC_DRAW);
     //	vbo.setTexCoordData(tex_coord, total, GL_DYNAMIC_DRAW);
-
+    count = 0;
+    
 }
 
 //--------------------------------------------------------------
@@ -43,50 +47,52 @@ void testApp::update(){
         //
         
         
-        ofVec3f vec(0,//ofSignedNoise(t, pos[j].y/div, pos[j].z/div),
+        ofVec3f _vec(ofSignedNoise(t, pos[j].y/div, pos[j].z/div),
                     ofSignedNoise(pos[j].x/div, t, pos[j].z/div),
-                    ofSignedNoise(pos[j].x/div, pos[j].y/div, t));
-        vec *=  ofGetLastFrameTime()*1000;
-        vec.x+=ofGetLastFrameTime()*1000;
-        
-        if(pos[j].y<-10)
+                    0);
+        _vec *=  ofGetLastFrameTime()*50;
+//        vec.x+=ofGetLastFrameTime()*1000;
+        vec[j]+=_vec;
+        vec[j]+=acc[j];
+        vec[j]*=0.9;
+        if(pos[j].y<-200)
         {
-            pos[j].y = ofGetHeight();
+//            pos[j].y = ofGetHeight();
             for (int i=0; i<LOC_LENGTH; i++)
             {
                 int index = i+(j*LOC_LENGTH);
                 loc[index].set(pos[j]);
             }
         }
-        else if(pos[j].x<-10)
+        else if(pos[j].x<-200)
         {
             
-            pos[j].x = ofGetWidth();
+//            pos[j].x = ofGetWidth();
             for (int i=0; i<LOC_LENGTH; i++)
             {
                 int index = i+(j*LOC_LENGTH);
                 loc[index].set(pos[j]);
             }
         }
-        else if(pos[j].y>ofGetHeight()+10)
+        else if(pos[j].y>ofGetHeight()+200)
         {
-            pos[j].y = 0;
+//            pos[j].y = 0;
             for (int i=0; i<LOC_LENGTH; i++)
             {
                 int index = i+(j*LOC_LENGTH);
                 loc[index].set(pos[j]);
             }
         }
-        else if(pos[j].x>ofGetWidth()+10)
+        else if(pos[j].x>ofGetWidth()+200)
         {
-            pos[j].x = 0;
+//            pos[j].x = 0;
             for (int i=0; i<LOC_LENGTH; i++)
             {
                 int index = i+(j*LOC_LENGTH);
                 loc[index].set(pos[j]);
             }
         }
-        else
+        else if(age[j]>0)
         {
             ofVec3f Off;
             float radius = 10;
@@ -99,98 +105,85 @@ void testApp::update(){
             {
                 int index = i+(j*LOC_LENGTH);
                 int index2 = (i*2)+(j*LENGTH);
-//                loc[index].set(loc[index-1]);s
-                //
-//                if(i>1)
-                radius = sinf(PI*float(i*1.f/LOC_LENGTH))*5;
+                
+
+                radius = sinf(PI*float(i*1.f/LOC_LENGTH))*2;
                 {
                     ofVec3f perp0 = loc[index] - loc[index+1];
                     ofVec3f perp1 = perp0.getCrossed( ofVec3f( 0, 1, 0 ) ).getNormalized();
                     ofVec3f perp2 = perp0.getCrossed( perp1 ).getNormalized();
                     perp1 = perp0.getCrossed( perp2 ).getNormalized();
-                    Off.x        = perp1.x * radius;// * agePer * per * .05;
-                    Off.y       = perp1.y * radius;// * agePer * per * .05;
-                    Off.z        = perp1.z * radius;// * agePer * per * .05;
+                    Off.x        = perp1.x * radius*age[j];// * agePer * per * .05;
+                    Off.y       = perp1.y * radius*age[j];// * agePer * per * .05;
+                    Off.z        = perp1.z * radius*age[j];// * agePer * per * .05;
                     
                     strip[(index2)]=loc[index]-Off;
                     
                     strip[(index2+1)]=loc[index]+Off;
                 }
-                //                }
-                //                if(i==1)
-                //                {
-                //                    strip[j*LENGTH]=loc[index];
-                ////                    strip[j*LENGTH-1]=loc[index];
-                //                }
-                //                else if(i==LOC_LENGTH-1)
-                //                {
-                //                    strip[(j*LENGTH)+(index2+1)]=loc[index];
-                //                }
-                
-                
+            }
+            loc[j*LOC_LENGTH] = pos[j];
+            pos[j]+=vec[j];
+            age[j]-=0.01;
+        }
+        else
+        {
+            for (int i=0; i<LOC_LENGTH; i++)
+            {
+                int index = i+(j*LOC_LENGTH);
+                loc[index].set(pos[j]);
             }
         }
-        loc[j*LOC_LENGTH] = pos[j];
-        pos[j]+=vec;
-//        pos[j].x += vec.x;
-//        pos[j].y += vec.y;
+        
         
     }
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+    
     vbo.bind();
 	vbo.updateVertexData(strip, total);
 	vbo.updateColorData(color, total);
-//	vbo.updateTexCoordData(tex_coord, total);
-
+    
+    
     for (int j=0; j<NUM_STRIP; j++)
-
+        
     {
         int index = j * LENGTH;
-
+        
         vbo.draw(GL_TRIANGLE_STRIP, index,LENGTH);
-//        vbo.draw(GL_LINE_STRIP, index,LENGTH);
+        
     }
-
+    
+    
 	vbo.unbind();
     
-
+    
     if(ofGetLogLevel()==OF_LOG_VERBOSE)
     {
-    for (int j=0; j<NUM_STRIP; j++)
-    {
-        ofSetColor(255);
-        ofCircle(pos[j] , 10);
-        for (int i=LENGTH-1; i>=0; i--)
+        for (int j=0; j<NUM_STRIP; j++)
         {
-            int index = i+(j*LENGTH);
-            if(i%2==0)
+            ofSetColor(255);
+            ofCircle(pos[j] , 10);
+            for (int i=LENGTH-1; i>=0; i--)
             {
-                ofSetColor(255, 0, 0);
-                ofCircle(strip[index] , 2);
-//                ofDrawBitmapString(ofToString(i), strip[index]);
-            }
-            else
-            {
-                ofSetColor(0, 0, 255);
-                ofCircle(strip[index] , 2);
-//                ofDrawBitmapString(ofToString(i), strip[index]);                
+                int index = i+(j*LENGTH);
+                if(i%2==0)
+                {
+                    ofSetColor(255, 0, 0);
+                    ofCircle(strip[index] , 2);
+                    
+                }
+                else
+                {
+                    ofSetColor(0, 0, 255);
+                    ofCircle(strip[index] , 2);
+                }
             }
         }
-        //        for (int i=LOC_LENGTH-1; i>=1; i--)
-        //        {
-        //            int index = i+(j*LOC_LENGTH);
-        //
-        //            ofCircle(loc[index] , 2);
-        //        }
-        
-        
     }
-    }
-
+    
 }
 
 //--------------------------------------------------------------
@@ -216,7 +209,25 @@ void testApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
+    count++;
     
+    count%=NUM_STRIP;
+    pos[count].set(x+ofRandom(-50,50), y+ofRandom(-50,50));
+    vec[count].set(x-pMouse.x,y-pMouse.y,0);
+    vec[count]*=2;
+    acc[count].set((x-pos[count].x)*0.01, (y-pos[count].y)*0.01);
+    age[count] = 1;
+//    float h = 255*ofRandom(0,1);
+    for (int i=0; i<LOC_LENGTH; i++)
+    {
+        int index = i+(count*LOC_LENGTH);
+        loc[index].set(pos[count]);
+        
+//        float brightness = ((LOC_LENGTH-i*1.0)/LOC_LENGTH)*255;
+//        color[index].set(ofColor::fromHsb(h,255, 255,brightness));
+    }
+    pMouse.set(x,y);
+
 }
 
 //--------------------------------------------------------------
--- src/testApp.h
+++ src/testApp.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "ofMain.h"
-#define NUM_STRIP 200
-#define LENGTH 20
+#define NUM_STRIP 1000
 #define LOC_LENGTH 10
+#define LENGTH LOC_LENGTH*2
+
 class testApp : public ofBaseApp{
 
 	public:
@@ -22,6 +23,9 @@ class testApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
 		ofVbo vbo;
     ofVec3f pos[NUM_STRIP];
+    ofVec3f acc[NUM_STRIP];
+        ofVec3f vec[NUM_STRIP];
+    float age[NUM_STRIP];
     
     
     ofVec3f strip[NUM_STRIP*LENGTH];
@@ -30,4 +34,6 @@ class testApp : public ofBaseApp{
 	ofFloatColor color[NUM_STRIP*LENGTH];
     int total;
 //    ofEasyCam cam;
+        int count;
+    ofVec3f pMouse;
 };
